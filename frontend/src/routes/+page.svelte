c0ba1bca18ade3d033dcc87c81b95b272edb85a5
Benjamin Breuil <benjamin.breuil@jobtrek.ch>
2023-12-14 14:57:32 +0100

    Update docker compose

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../styles/global.css'
+</script>
+<main>
+    <h1 class="text-6xl">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
